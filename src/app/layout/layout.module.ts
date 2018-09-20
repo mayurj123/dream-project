d3cc70d8adaa1b58933cf8b225febe7bc06510b3
milindg <milind.g@nextremer.com>
2018-09-20 17:24:07 +0530

    Add sub component and header design.

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { HeaderComponent } from './header/header.component';
+import { HeaderSearchComponent } from './header/header-search/header-search.component';
+import { HeaderCartComponent } from './header/header-cart/header-cart.component';
+import { ProfileDropdownComponent } from './header/profile-dropdown/profile-dropdown.component';
 
 @NgModule({
   imports: [
@@ -11,6 +14,6 @@ import { HeaderComponent } from './header/header.component';
   exports: [
     HeaderComponent
   ],
-  declarations: [HeaderComponent]
+  declarations: [HeaderComponent, HeaderSearchComponent, HeaderCartComponent, ProfileDropdownComponent]
 })
 export class LayoutModule { }
