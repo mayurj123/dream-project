2e3f7dd5a106cdc9a233cfea7704d0e19cbef5a3
milindg <milind.g@nextremer.com>
2018-09-21 15:51:06 +0530

    Resolve multi module routing issue.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthRoutingModule } from './auth-routing.module';
+import { AuthRoutes as routes } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
-    AuthRoutingModule
+    RouterModule.forChild(routes),
   ],
   declarations: [LoginComponent]
 })
