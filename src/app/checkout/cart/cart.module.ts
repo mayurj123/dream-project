1d5fc2de79a40d4c9dc710c1bd3b9639c8a43d86
milindg <milind.g@nextremer.com>
2018-09-24 18:17:24 +0530

    Add cart-item-list component in checkout module.

--- src/app/checkout/cart/cart.module.ts
+++ src/app/checkout/cart/cart.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmptyCartComponent } from './empty-cart/empty-cart.component';
 import { RouterModule } from '@angular/router';
+import { CartItemListComponent } from './cart-item-list/cart-item-list.component';
+import { OrderTotalSummaryComponent } from './order-total-summary/order-total-summary.component';
 
 @NgModule({
   imports: [
@@ -9,8 +11,10 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ],
   exports:[
-    EmptyCartComponent
+    EmptyCartComponent,
+    CartItemListComponent,
+    OrderTotalSummaryComponent
   ],
-  declarations: [EmptyCartComponent]
+  declarations: [EmptyCartComponent, CartItemListComponent, OrderTotalSummaryComponent]
 })
 export class CartModule { }
