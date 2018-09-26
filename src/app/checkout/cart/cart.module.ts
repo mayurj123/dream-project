b026ce20d26c0addd1996f1d2ee11f38da1cee38
milindg <milind.g@nextremer.com>
2018-09-26 13:17:17 +0530

    Add product-list component in landing module.

--- src/app/checkout/cart/cart.module.ts
+++ src/app/checkout/cart/cart.module.ts
@@ -4,11 +4,13 @@ import { EmptyCartComponent } from './empty-cart/empty-cart.component';
 import { RouterModule } from '@angular/router';
 import { CartItemListComponent } from './cart-item-list/cart-item-list.component';
 import { OrderTotalSummaryComponent } from './order-total-summary/order-total-summary.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MDBBootstrapModule.forRoot()
   ],
   exports:[
     EmptyCartComponent,
--- src/app/checkout/checkout.module.ts
+++ src/app/checkout/checkout.module.ts
@@ -5,12 +5,14 @@ import { CartComponent } from './cart/cart.component';
 import { RouterModule } from '@angular/router';
 import { routes } from './checkout-routing.module';
 import { CartModule } from './cart/cart.module';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    CartModule
+    CartModule,
+    MDBBootstrapModule.forRoot()
   ],
   declarations: [CartComponent]
 })
